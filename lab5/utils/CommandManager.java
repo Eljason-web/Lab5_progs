f8124fc5d1a10aa0de344d671b0c402a97fd5a11
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-22 22:12:54 +0300

    Update CommandManager.java
    moved addToHistory below the swtich logic

--- lab5/utils/CommandManager.java
+++ lab5/utils/CommandManager.java
@@ -24,70 +24,56 @@ public class CommandManager {
         switch (c) {
             case "show":
                 new Show(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "help":
                 new Help().execute();
-                addToHistory(c);
                 break;
             case "info":
                 new Info(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "exit":
                 new Exit().execute();
-                addToHistory(c);
                 break;
             case "save":
                 new Save(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "add":
                 new Add(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "clear":
                 new Clear(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "history":
                 new History(commandHistory).execute();
-                addToHistory(c);
                 break;
             case "add_if_min":
                 new AddIfMin(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "add_if_max":
                 new AddIfMax(collectionManager).execute();
-                addToHistory(c);
             case "update":
                 new Update(collectionManager).execute(arg);
-                addToHistory(c);
                 break;
             case "remove_by_id":
                 new RemoveById(collectionManager).execute(arg);
-                addToHistory(c);
                 break;
             case "execute_script":
                 new ExecuteScript(collectionManager).execute(arg);
-                addToHistory(c);
                 break;
             case "count_by_agglomeration":
                 new CountByAgglomeration(collectionManager).execute(arg);
-                addToHistory(c);
                 break;
             case "print_field_ascending_meters_above_sea_level":
                 new PrintFieldAscendingMetersAboveSeaLevel(collectionManager).execute();
-                addToHistory(c);
                 break;
             case "filter_by_government":
                 new FilterByGovernment(collectionManager).execute(arg);
-                addToHistory(c);
                 break;
             default:
                 System.out.println("Unknown command. Enter help for a list of commands");
         }
+
+        addToHistory(c);
 
         System.out.println();
     }
