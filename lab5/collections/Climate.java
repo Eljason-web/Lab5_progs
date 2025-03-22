aa08430ec092a3b964420e7474f162b5ca71a012
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-22 22:04:25 +0300

    Delete lab5 directory

--- lab5/collections/Climate.java
+++
@@ -1,7 +0,0 @@
-package org.example.collections;
-
-public enum Climate {
-    TROPICAL_SAVANNA,
-    HUMID_CONTINENTAL,
-    STEPPE
-}
--- lab5/collections/Government.java
+++
@@ -1,8 +0,0 @@
-package org.example.collections;
-
-public enum Government {
-    DESPOTISM,
-    NOOCRACY,
-    TIMOCRACY;
-
-}
