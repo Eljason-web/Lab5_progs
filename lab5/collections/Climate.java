752c11c7762a7fbf7728e23da1f4b9afa4adf871
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-22 22:19:07 +0300

    Update Climate.java

--- lab5/collections/Climate.java
+++ lab5/collections/Climate.java
@@ -1,22 +1,7 @@
 package org.example.collections;
 
 public enum Climate {
-    TROPICAL_SAVANNA {
-        @Override
-        public String toString() {
-            return super.toString();
-        }
-    },
-    HUMIDCONTINENTAL{
-        @Override
-        public String toString() {
-            return super.toString();
-        }
-    },
-    STEPPE{
-        @Override
-        public String toString() {
-            return super.toString();
-        }
-    }
+    TROPICAL_SAVANNA,
+    HUMIDCONTINENTAL,
+    STEPPE
 }
