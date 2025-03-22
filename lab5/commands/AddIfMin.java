0b5d4e312937694fbf4ddf9fd3894d5e1ad18b24
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-22 22:16:50 +0300

    Update AddIfMin.java

--- lab5/commands/AddIfMin.java
+++ lab5/commands/AddIfMin.java
@@ -1,9 +1,7 @@
 package org.example.commands;
 
-import org.example.collections.City;
 import org.example.utils.CollectionManager;
 
-import java.util.Scanner;
 
 public class AddIfMin implements Commands{
     private final CollectionManager collectionManager;
@@ -14,19 +12,7 @@ public class AddIfMin implements Commands{
 
     @Override
     public void execute() {
-        Scanner scanner = new Scanner(System.in);
-        City newCity = collectionManager.readCityFromScanner(scanner);
-
-        if(collectionManager.getCities().isEmpty()) {
-            collectionManager.add(newCity);
-        } else {
-            int currentMinId = collectionManager.getCities().getFirst().getId();
-            if(newCity.getId() < currentMinId) {
-                collectionManager.add(newCity);
-            } else {
-                System.out.println("New city is not minimum and cannot be added");
-            }
-        }
+        collectionManager.addIfMin();
     }
 
     @Override
