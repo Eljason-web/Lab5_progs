2b14cd45e11ef89043aa5710316ea7c02a56a5a9
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-22 22:17:19 +0300

    Update AddIfMax.java

--- lab5/commands/AddIfMax.java
+++ lab5/commands/AddIfMax.java
@@ -1,32 +1,18 @@
 package org.example.commands;
 
-import org.example.collections.City;
 import org.example.utils.CollectionManager;
 
-import java.util.Scanner;
 
-public class AddIfMax implements Commands{
+public class AddIfMin implements Commands{
     private final CollectionManager collectionManager;
 
-    public AddIfMax(CollectionManager collectionManager) {
+    public AddIfMin(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
     }
 
     @Override
     public void execute() {
-        Scanner scanner = new Scanner(System.in);
-        City newCity = collectionManager.readCityFromScanner(scanner);
-
-        if(collectionManager.getCities().isEmpty()) {
-            collectionManager.add(newCity);
-        } else {
-            int currentMaxId = collectionManager.getCities().getLast().getId();
-            if(newCity.getId() > currentMaxId) {
-                collectionManager.add(newCity);
-            } else {
-                System.out.println("New city is not maximum and cannot be added");
-            }
-        }
+        collectionManager.addIfMin();
     }
 
     @Override
