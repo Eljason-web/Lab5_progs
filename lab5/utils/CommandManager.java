509f1ebbc95f14376031ee1ec08d63dcd66374ec
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-27 18:01:10 +0300

    Update CommandManager.java

--- lab5/utils/CommandManager.java
+++ lab5/utils/CommandManager.java
@@ -1,16 +1,35 @@
 package org.example.utils;
-
 import org.example.commands.*;
-
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandManager {
     private final CollectionManager collectionManager;
     private final ArrayDeque<String> commandHistory = new ArrayDeque<>();
+    private final Map<String, Commands> commandsMap = new HashMap<>();
 
     public CommandManager(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
+
+        commandsMap.put("show", new Show(collectionManager));
+        commandsMap.put("help", new Help());
+        commandsMap.put("info", new Info(collectionManager));
+        commandsMap.put("exit", new Exit());
+        commandsMap.put("save", new Save(collectionManager));
+        commandsMap.put("add", new Add(collectionManager));
+        commandsMap.put("clear", new Clear(collectionManager));
+        commandsMap.put("history", new History(commandHistory));
+        commandsMap.put("add_if_min", new AddIfMin(collectionManager));
+        commandsMap.put("add_if_max", new AddIfMax(collectionManager));
+        commandsMap.put("update", new Update(collectionManager));
+        commandsMap.put("remove_by_id", new RemoveById(collectionManager));
+        commandsMap.put("execute_script", new ExecuteScript(collectionManager, this));
+        commandsMap.put("count_by_agglomeration", new CountByAgglomeration(collectionManager));
+        commandsMap.put("print_field_ascending_meters_above_sea_level", new PrintFieldAscendingMetersAboveSeaLevel(collectionManager));
+        commandsMap.put("filter_by_government", new FilterByGovernment(collectionManager));
     }
+
 
     public void executeCommand(String command) {
         String[] commandArray = command.split(" ");
@@ -21,59 +40,18 @@ public class CommandManager {
             arg = commandArray[1];
         }
 
-        switch (c) {
-            case "show":
-                new Show(collectionManager).execute();
-                break;
-            case "help":
-                new Help().execute();
-                break;
-            case "info":
-                new Info(collectionManager).execute();
-                break;
-            case "exit":
-                new Exit().execute();
-                break;
-            case "save":
-                new Save(collectionManager).execute();
-                break;
-            case "add":
-                new Add(collectionManager).execute();
-                break;
-            case "clear":
-                new Clear(collectionManager).execute();
-                break;
-            case "history":
-                new History(commandHistory).execute();
-                break;
-            case "add_if_min":
-                new AddIfMin(collectionManager).execute();
-                break;
-            case "add_if_max":
-                new AddIfMax(collectionManager).execute();
-            case "update":
-                new Update(collectionManager).execute(arg);
-                break;
-            case "remove_by_id":
-                new RemoveById(collectionManager).execute(arg);
-                break;
-            case "execute_script":
-                new ExecuteScript(collectionManager).execute(arg);
-                break;
-            case "count_by_agglomeration":
-                new CountByAgglomeration(collectionManager).execute(arg);
-                break;
-            case "print_field_ascending_meters_above_sea_level":
-                new PrintFieldAscendingMetersAboveSeaLevel(collectionManager).execute();
-                break;
-            case "filter_by_government":
-                new FilterByGovernment(collectionManager).execute(arg);
-                break;
-            default:
-                System.out.println("Unknown command. Enter help for a list of commands");
-        }
+        Commands commandInstance = commandsMap.get(c);
 
-        addToHistory(c);
+        if (commandInstance != null) {
+            if (arg.isEmpty()) {
+                commandInstance.execute();
+            } else {
+                commandInstance.execute(arg);
+            }
+            addToHistory(c);
+        } else {
+            System.out.println("Unknown command. Enter 'help' for a list of commands.");
+        }
 
         System.out.println();
     }
