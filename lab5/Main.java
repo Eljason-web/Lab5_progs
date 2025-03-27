d6b5b3b9441dec6ab34e2a4974c36ea03341f8d7
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-27 17:58:14 +0300

    Update Main.java

--- lab5/Main.java
+++ lab5/Main.java
@@ -1,31 +1,34 @@
 package org.example;
 
-import org.example.collections.*;
-import org.example.utils.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Scanner;
+import org.example.collections.CityCollection;
+import org.example.utils.CollectionManager;
+import org.example.utils.CommandManager;
+import org.example.utils.ConsoleManager;
+import org.example.utils.FileManager;
+
 
 
 public class Main {
         public static void main(String[] args) {
-                CollectionManager collectionManager = new CollectionManager();
-                FileManager fileManager = new FileManager(collectionManager);
+            CityCollection cityCollection = new CityCollection();
+            CollectionManager collectionManager = new CollectionManager(cityCollection);
+            FileManager fileManager = new FileManager(collectionManager);
 
-                String fileName = System.getenv("COLLECTION_FILE_PATH");
+            String fileName = System.getenv("FILE_COLLECTION_PATH");
 
-                if (fileName == null || fileName.isEmpty()) {
-                        System.out.println("Environment variable COLLECTION_FILE_PATH is not set or is empty.");
-                        return;
-                }
+
+            if (fileName == null || fileName.isEmpty()) {
+                    System.out.println("Environment variable FILE_COLLECTION_PATH is not set or is empty.");
+                    return;
+            }
 
                 fileManager.loadCollectionFromXml(fileName); //load using environmental variable
-//               fileManager.loadCollectionFromXml(args[0]); //load  using command line variable
-//                fileManager.loadCollectionFromXml("storage.xml"); //to load using name of file directly
+//              fileManager.loadCollectionFromXml(args[0]); //load  using command line variable
+//              fileManager.loadCollectionFromXml("storage.xml"); //to load using name of file directly
 
 
-                CommandManager commandManager = new CommandManager(collectionManager);
-                ConsoleManager consoleManager = new ConsoleManager(commandManager);
-                consoleManager.interactiveMode(); //triggers interactive mode with user
+            CommandManager commandManager = new CommandManager(collectionManager);
+            ConsoleManager consoleManager = new ConsoleManager(commandManager);
+            consoleManager.interactiveMode(); //triggers interactive mode with user
         }
 }
