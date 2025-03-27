2024be0ae42f8c89d0a6b94cee778716a4814381
Eljason-web <enochjasonlarbi@gmail.com>
2025-03-27 19:46:00 +0300

    Update Update.java

--- lab5/commands/Update.java
+++ lab5/commands/Update.java
@@ -1,7 +1,10 @@
 package org.example.commands;
 
-import org.example.collections.City;
+import org.example.collections.*;
+import org.example.utils.CityReader;
 import org.example.utils.CollectionManager;
+
+import java.util.Scanner;
 
 public class Update implements Commands{
     private final CollectionManager collectionManager;
@@ -19,18 +22,15 @@ public class Update implements Commands{
     public void execute(String arg) {
         try{
             int id = Integer.parseInt(arg);
-            boolean idFound = false;
-
-            for(City city : collectionManager.getCities()){
-                if(city.getId() == id) {
-                    idFound = true;
-                    collectionManager.updateCity(city);
-                    break;
-                }
-            }
-            
-            if(!idFound){
+            City cityWithId = collectionManager.findCityById(id);
+            if(cityWithId == null){
                 System.out.println("City with ID " + arg + " does not exist");
+            } else {
+                Scanner scanner = new Scanner(System.in);
+                CityReader cityReader = new CityReader(scanner);
+                City cityUpdates = cityReader.collectCityData();
+                collectionManager.updateCity(cityWithId, cityUpdates);
+                System.out.println("City with id: " + cityWithId.getId() + " has been updated");
             }
         } catch (Exception e){
             System.out.println("Invalid Id format");
