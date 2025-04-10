3d53c4770e1ee841688e2d54ad167633f04691c7
Eljason-web <enochjasonlarbi@gmail.com>
2025-04-10 12:54:57 +0300

    Update CollectionManager.java

--- lab5/utils/CollectionManager.java
+++ lab5/utils/CollectionManager.java
@@ -9,93 +9,64 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-
-
 public class CollectionManager {
     private final CityCollection cityCollection;
+    private final String collectionFilePath;
 
-    public CollectionManager(CityCollection cityCollection) {
+    public CollectionManager(CityCollection cityCollection, String collectionFilePath) {
         this.cityCollection = cityCollection;
+        this.collectionFilePath = collectionFilePath;
     }
 
     public CityCollection getCityCollection() {
         return cityCollection;
     }
 
-    public void setCityCollection(ArrayDeque<City> loadedCollection) {
-        if(loadedCollection != null) {
-           cityCollection.addAllCities(loadedCollection);
-        } else {
-            System.out.println("Warning: Attempted to load null collection");
-        }
+    public Map<String, String> info() throws Exception {
+        Map<String, String> infoMap = new HashMap<>();
+        infoMap.put("initialization date", getInitializationDate());
+        infoMap.put("number of elements", String.valueOf(cityCollection.getCities().size()));
+        infoMap.put("collection type", String.valueOf(cityCollection.getCities().getClass()));
+
+        return infoMap;
+
     }
 
-    public String show() {
-        if (cityCollection.getCities().isEmpty()) {
-            return "We do not have any cities in our collection";
-        } else {
-            StringBuilder result = new StringBuilder("LIST OF CITIES IN THE COLLECTION:\n");
-            for (City city : cityCollection.getCities()) {
-                result.append(city.toString()).append("\n\n");
-            }
-            return result.toString();
-        }
-    }
-
-    public String info() {
-        String initializationDate = getInitializationDate();
-        int numberOfElements = cityCollection.getCities().size();
-        return "Information about collection\n" +
-                "Type: " + cityCollection.getCities().getClass() + "\n" +
-                "Initialization date: " + initializationDate + "\n" +
-                "Number of cities: " + numberOfElements;
-    }
-
-    private String getInitializationDate() {
-        String date = "";
+    private String getInitializationDate() throws Exception {
         try {
-            String fileName = System.getenv("FILE_COLLECTION_PATH");
-            Path path = Paths.get(fileName);
+            Path path = Paths.get(collectionFilePath);
             BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
             FileTime fileTime = attrs.creationTime();
             Instant instant = fileTime.toInstant();
             LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-            date = localDateTime.format(formatter);
-
+            return localDateTime.format(formatter);
         } catch (IOException e) {
-            System.out.println("exception: " + e.getMessage());
+            throw new Exception("exception: " + e.getMessage());
         }
-        return date;
     }
 
     public void add(City city) {
-        //for automatically generated values
         int id = generateId();
-        LocalDate creationDate = LocalDate.now();
-
         city.setId(id);
-        city.setCreationDate(creationDate);
-
         cityCollection.addCity(city);
     }
 
     private int generateId() {
-        if(cityCollection.getCities().isEmpty()) {
+        if (cityCollection.getCities().isEmpty()) {
             return 1;
         }
         return cityCollection.getCities().getLast().getId() + 1; // works because array deque is already sorted
     }
 
-    public City findCityById(int id){
-        for(City city : cityCollection.getCities()){
-            if(city.getId() == id) {
+    public City findCityById(int id) {
+        for (City city : cityCollection.getCities()) {
+            if (city.getId() == id) {
                 return city;
             }
         }
@@ -103,7 +74,7 @@ public class CollectionManager {
     }
 
     public void clearCollection() {
-            cityCollection.clearCities();
+        cityCollection.clearCities();
     }
 
     public void updateCity(City originalCity, City cityUpdates) {
@@ -116,7 +87,6 @@ public class CollectionManager {
         originalCity.setClimate(cityUpdates.getClimate());
         originalCity.setGovernment(cityUpdates.getGovernment());
         originalCity.setGovernor(cityUpdates.getGovernor());
-
 
     }
 
@@ -138,6 +108,3 @@ public class CollectionManager {
     }
 
 }
-
-
-
