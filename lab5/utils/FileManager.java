0234ef2f1feba759dff962412460f753f16d5577
Eljason-web <enochjasonlarbi@gmail.com>
2025-04-10 13:05:15 +0300

    Update FileManager.java

--- lab5/utils/FileManager.java
+++ lab5/utils/FileManager.java
@@ -1,7 +1,5 @@
 package org.example.utils;
 
-
-import org.example.collections.City;
 import org.example.collections.CityCollection;
 
 import javax.xml.bind.JAXBContext;
@@ -9,17 +7,11 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class FileManager {
-    private final CollectionManager collectionManager;
 
-    public FileManager(CollectionManager collectionManager) {
-        this.collectionManager = collectionManager;
-    }
-
-    public void saveToXml(String filePath){
+    public void saveToXml(String filePath, CityCollection cityCollection) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
             // Create JAXB context for the CollectionManager class
             JAXBContext context = JAXBContext.newInstance(CityCollection.class);
@@ -29,7 +21,7 @@ public class FileManager {
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
             // Marshal the CityCollection object to XML and save it to the file
-            marshaller.marshal(collectionManager.getCityCollection(), writer);
+            marshaller.marshal(cityCollection, writer);
 
             System.out.println("Collection saved to " + filePath);
         } catch (JAXBException e) {
@@ -39,29 +31,26 @@ public class FileManager {
         }
     }
 
-    public void loadCollectionFromXml(String filePath) {
-        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filePath))){
+    public CityCollection loadCollectionFromXml(String filePath) {
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filePath))) {
             // Create a JAXB context for City Collection class
             JAXBContext context = JAXBContext.newInstance(CityCollection.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             // Deserialize from XML file
             CityCollection cityCollection = (CityCollection) unmarshaller.unmarshal(inputStream);
-            System.out.println('4');
-
-            // Get the TreeSet of Persons
-            ArrayDeque<City> loadedCollection = cityCollection.getCities();
-            collectionManager.setCityCollection(loadedCollection);
             System.out.println("Collection loaded successfully!");
+            return cityCollection;
         } catch (JAXBException e) {
             System.out.println("Error loading collection: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Error reading from file: " + e.getMessage());
         }
+        return null;
     }
 
-    public ArrayList<String> loadCommandsFromScript(String filePath){
+    public ArrayList<String> loadCommandsFromScript(String filePath) {
         ArrayList<String> commands = new ArrayList<>();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
             String command;
             while ((command = bufferedReader.readLine()) != null) {
                 commands.add(command);
