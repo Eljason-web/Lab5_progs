f8be1b9b6513c9aad39a99b02b5373fba111a0e6
Eljason-web <enochjasonlarbi@gmail.com>
2025-04-10 13:15:00 +0300

    Update Add.java

--- lab5/commands/Add.java
+++ lab5/commands/Add.java
@@ -1,19 +1,17 @@
 package org.example.commands;
 
-
 import org.example.collections.City;
 import org.example.utils.CityReader;
 import org.example.utils.CollectionManager;
 
 import java.util.Scanner;
 
-public class Add implements Commands{
+public class Add implements Commands {
     private final CollectionManager collectionManager;
 
     public Add(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
     }
-
 
     @Override
     public void execute() {
@@ -22,27 +20,6 @@ public class Add implements Commands{
         City newCity = cityReader.collectCityData();
         collectionManager.add(newCity);
         System.out.println("City added: " + newCity.getName());
-    }
-
-    @Override
-    public void execute(String arg) {
-
-    }
-}package org.example.commands;
-
-import org.example.utils.CollectionManager;
-
-public class Add implements Commands{
-    private final CollectionManager collectionManager;
-
-    public Add(CollectionManager collectionManager) {
-        this.collectionManager = collectionManager;
-    }
-
-
-    @Override
-    public void execute() {
-       collectionManager.addCity();
     }
 
     @Override
