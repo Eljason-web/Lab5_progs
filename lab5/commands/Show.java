9d966d4f8f0cc5911976e08a2e4982c388f098ff
Eljason-web <enochjasonlarbi@gmail.com>
2025-04-11 06:20:02 +0300

    Update Show.java

--- lab5/commands/Show.java
+++ lab5/commands/Show.java
@@ -1,8 +1,9 @@
 package org.example.commands;
 
+import org.example.collections.City;
 import org.example.utils.CollectionManager;
 
-public class Show implements Commands{
+public class Show implements Commands {
     private final CollectionManager collectionManager;
 
     public Show(CollectionManager collectionManager) {
@@ -16,7 +17,16 @@ public class Show implements Commands{
 
     @Override
     public void execute() {
-        String showResult = collectionManager.show();
-        System.out.println(showResult);
+        if (collectionManager.getCityCollection().getCities().isEmpty()) {
+            System.out.println("We do not have any cities in our collection");
+        } else {
+            StringBuilder result = new StringBuilder("LIST OF CITIES IN THE COLLECTION:\n");
+
+            for (City city : collectionManager.getCityCollection().getCities()) {
+                result.append(city.toString()).append("\n\n");
+            }
+
+            System.out.println(result);
+        }
     }
 }
