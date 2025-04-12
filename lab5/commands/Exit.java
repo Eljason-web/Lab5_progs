aed57e2fd81b05ae28588365e199883920c31ada
Eljason-web <enochjasonlarbi@gmail.com>
2025-04-12 12:43:56 +0300

    Update Exit.java

--- lab5/commands/Exit.java
+++ lab5/commands/Exit.java
@@ -3,7 +3,6 @@ package org.example.commands;
 public class Exit implements Commands {
     public void execute() {
         System.out.println("PROGRAM CLOSING...");
-        System.exit(-1);
     }
 
     @Override
